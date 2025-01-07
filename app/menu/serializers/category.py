3f586d8ff8d7975d093c7dbcb9e2af54121a0b2a
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-01-07 06:12:40 +0600

    add: (api for category)

--- app/menu/serializers/category.py
+++ app/menu/serializers/category.py
@@ -6,5 +6,4 @@ from ..models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['category_id', 'category_name', 'category_photo', 'category_photo_origin',
-                  'category_hidden', 'level', 'venue']
+        fields = ['category_id', 'category_name', 'category_photo', 'category_photo_origin']
