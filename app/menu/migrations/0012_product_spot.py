50fbf1bd49c480ee72f52f9ffe6952c6ed270542
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-02-12 09:20:34 +0600

    add: filter by spot and forget password

--- app/menu/migrations/0012_product_spot.py
+++ app/menu/migrations/0012_product_spot.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='product',
             name='spot',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='products', to='venues.spot', verbose_name='Точка заведения'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='products', to='venues.spot', verbose_name='Точка заведения'),
             preserve_default=False,
         ),
     ]
