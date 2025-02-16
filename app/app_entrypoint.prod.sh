1701294de86f6a7ed4e201b175007738fc81c65c
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-02-16 13:49:33 +0600

    refact: deploy with asgi

--- app/app_entrypoint.prod.sh
+++ app/app_entrypoint.prod.sh
@@ -4,4 +4,5 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-gunicorn config.wsgi --reload --workers 2 --bind 0.0.0.0:8000 --timeout 120 --log-level info
+#gunicorn config.wsgi --reload --workers 2 --bind 0.0.0.0:8000 --timeout 120 --log-level info
+gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --workers 2 --log-level info
