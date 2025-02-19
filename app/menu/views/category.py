a15a1a0d137fe9d3e20f15d25ef5eb3da0d25a1a
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-02-20 04:59:07 +0600

    add: filter by venue and spot

--- app/menu/views/category.py
+++ app/menu/views/category.py
@@ -1,10 +1,31 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins
 from ..models import Category
 from ..serializers import CategorySerializer
 
-@extend_schema(tags=['Сategory',])
+@extend_schema(
+    tags=['Сategory',],
+    parameters=[
+        OpenApiParameter(
+            name='venue_name',  # Имя параметра
+            description='Фильтр по имени заведения',  # Описание параметра
+            required=False,  # Параметр необязательный
+            type=str  # Тип данных
+        ),
+    ]
+)
 class CategoryViewSet(viewsets.GenericViewSet,
                       mixins.ListModelMixin):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+    def get_queryset(self):
+        queryset = Category.objects.select_related('venue')
+        venue_name = self.request.GET.get("venue_name")
+
+        if venue_name:
+            queryset = queryset.filter(venue__company_name__icontains=venue_name)
+
+        queryset = queryset.distinct()
+        return queryset
--- app/orders/views/order.py
+++ app/orders/views/order.py
@@ -1,7 +1,7 @@
 import logging
 
 from django_filters.rest_framework import DjangoFilterBackend
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter
 from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
@@ -10,15 +10,34 @@ from services.pos_service_factory import POSServiceFactory
 from venues.models import Venue, Table
 from ..models import Order
 from ..serializers import OrderSerializer
-from ..websocket_utils import notify_order_status
 
 logger = logging.getLogger(__name__)
 
 
-@extend_schema(tags=['Order'])
+@extend_schema(
+    tags=['Order'],
+)
 @extend_schema_view(
-    create=extend_schema(summary='Создание заказа'),
-    list=extend_schema(summary='Получение заказов по id заведения и id стола')
+    create=extend_schema(
+        summary='Создание заказа',
+    ),
+    list=extend_schema(
+        summary='Получение заказов по названию заведения и номеру стола',
+        parameters=[
+            OpenApiParameter(
+                name='venue_name',  # Имя параметра
+                description='Фильтр по имени заведения',  # Описание параметра
+                required=False,  # Параметр необязательный
+                type=str  # Тип данных
+            ),
+            OpenApiParameter(
+                name='table_num',  # Имя параметра
+                description='Фильтр по номеру стола',  # Описание параметра
+                required=False,  # Параметр необязательный
+                type=str  # Тип данных
+            )
+        ]
+    )
 )
 class OrderViewSet(viewsets.GenericViewSet,
                    mixins.ListModelMixin,
@@ -26,7 +45,21 @@ class OrderViewSet(viewsets.GenericViewSet,
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['venue', 'table']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        venue_name = self.request.GET.get('venue_name', None)
+        table_num = self.request.GET.get('table_num', None)
+
+        if venue_name:
+            queryset = queryset.filter(venue__company_name__icontains=venue_name)
+
+        if table_num:
+            queryset = queryset.filter(table__table_num=table_num)
+
+        return queryset
+
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -37,8 +70,22 @@ class OrderViewSet(viewsets.GenericViewSet,
 
         try:
             order_data = serializer.validated_data
-            venue = Venue.objects.filter(id=4).first()
-            table = Table.objects.filter(venue=venue, table_num='8').first()
+
+            venue_name = request.data.get('venue_name')
+            table_num = request.data.get('table_num')
+
+            if not venue_name or not table_num:
+                return Response({'error': 'Venue name and table number are required.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            venue = Venue.objects.filter(company_name__icontains=venue_name).first()
+            if not venue:
+                return Response({'error': 'Venue not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+            table = Table.objects.filter(venue=venue, table_num=table_num).first()
+            if not table:
+                return Response({'error': 'Table not found.'}, status=status.HTTP_404_NOT_FOUND)
+
             api_token = venue.access_token
             pos_system_name = venue.pos_system.name.lower()
             pos_service = POSServiceFactory.get_service(pos_system_name, api_token)
