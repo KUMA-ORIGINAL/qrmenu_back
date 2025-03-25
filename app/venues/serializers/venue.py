9af1907a3ae5944d8bcb1e284830e60b14da83a6
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-25 09:51:15 +0600

    refact: endpoint for venue

--- app/venues/serializers/venue.py
+++ app/venues/serializers/venue.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from venues.models import Venue
+from ..models import Venue
+from .table import TableSerializer
 
 
 class VenueSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Venue
+        fields = ('color_theme', 'company_name', 'slug', 'logo', 'schedule')
+
+
+class VenueWithTableSerializer(serializers.ModelSerializer):
+    table = TableSerializer()
 
     class Meta:
         model = Venue
--- app/venues/views/venue.py
+++ app/venues/views/venue.py
@@ -1,6 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from venues.models import Venue
+from venues.models import Venue, Table
+from venues.serializers.table import TableSerializer
 from venues.serializers.venue import VenueSerializer
 
 
@@ -9,3 +13,17 @@ class VenueViewSet(viewsets.GenericViewSet,
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
     lookup_field = 'slug'
+
+    @action(detail=True, methods=['get'], url_path='table/(?P<table_id>[^/.]+)')
+    def retrieve_by_table_id(self, request, slug=None, table_id=None):
+        """
+        Получить заведение (`Venue`) + информацию о столе (`Table`) по `table_id`.
+        URL: `/api/venues/{slug}/table/{table_id}/`
+        """
+        venue = get_object_or_404(Venue, slug=slug)
+        table = get_object_or_404(Table, pk=table_id, venue=venue)  # Проверяем, что стол принадлежит заведению
+
+        venue_data = VenueSerializer(venue).data
+        venue_data['table'] = TableSerializer(table).data  # Добавляем данные о столе
+
+        return Response(venue_data)
