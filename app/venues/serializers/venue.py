9a80b07b50f290ec3ccf43eb5012b71c9596feb6
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-29 17:29:37 +0600

    refact: get venue by slug

--- app/venues/serializers/venue.py
+++ app/venues/serializers/venue.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 
+from .spot import SpotSerializer
 from ..models import Venue
 from .table import TableSerializer
 
 
 class VenueSerializer(serializers.ModelSerializer):
+    spots = SpotSerializer(many=True)
 
     class Meta:
         model = Venue
-        fields = ('color_theme', 'company_name', 'slug', 'logo', 'schedule')
+        fields = ('color_theme', 'company_name', 'slug', 'logo', 'schedule', 'spots')
 
 
 class VenueWithTableSerializer(serializers.ModelSerializer):
