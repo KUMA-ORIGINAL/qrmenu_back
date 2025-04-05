9ca772b2dbf4643964354498f510708b1e8f918d
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-04-05 17:27:51 +0600

    refact: admin panel

--- app/venues/admin/spot.py
+++ app/venues/admin/spot.py
@@ -7,6 +7,7 @@ from ..models import Spot
 
 @admin.register(Spot)
 class SpotAdmin(BaseModelAdmin):
+    search_fields = ('name',)
     change_form_before_template = 'venues/spot_change_form_before.html'
 
     def get_list_display(self, request):
