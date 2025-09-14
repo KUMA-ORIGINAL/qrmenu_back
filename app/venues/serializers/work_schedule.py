95832e443ca6046c94d7d0538206a11fb5a778c4
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-14 16:41:21 +0600

    feat: reset password

--- app/venues/serializers/work_schedule.py
+++ app/venues/serializers/work_schedule.py
@@ -4,6 +4,8 @@ from ..models import WorkSchedule
 
 class WorkScheduleSerializer(serializers.ModelSerializer):
     day_name = serializers.CharField(source="get_day_of_week_display", read_only=True)
+    work_start = serializers.TimeField(format="%H:%M", input_formats=["%H:%M"])
+    work_end = serializers.TimeField(format="%H:%M", input_formats=["%H:%M"])
 
     class Meta:
         model = WorkSchedule
