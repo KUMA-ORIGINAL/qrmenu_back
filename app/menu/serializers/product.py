b33058dafd76e60a539fe3a5ef3a112bfcc6e600
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-19 04:46:20 +0600

    feat: reset password

--- app/menu/serializers/product.py
+++ app/menu/serializers/product.py
@@ -36,17 +36,38 @@ class AbsoluteImageSerializerField(serializers.ImageField):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    # product_attributes = ProductAttributeSerializer(many=True, read_only=True)
     modificators = ModificatorSerializer(many=True, read_only=True)
     category = CategoryShortSerializer(read_only=True)
-
-    product_photo = AbsoluteImageSerializerField(read_only=True)
-    product_photo_small = AbsoluteImageSerializerField(read_only=True)
-    product_photo_large = AbsoluteImageSerializerField(read_only=True)
+    product_photo = serializers.SerializerMethodField(read_only=True)
+    product_photo_small = serializers.SerializerMethodField(read_only=True)
+    product_photo_large = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Product
         fields = [
             'id', 'product_name', 'product_description', 'product_price', 'weight',
-            'product_photo', 'product_photo_small', 'product_photo_large',
-            'category', 'is_recommended', 'modificators'
+            'product_photo', 'product_photo_small', 'product_photo_large', 'category', 'is_recommended', 'modificators'
         ]
+
+    def get_product_photo(self, obj):
+        if obj.product_photo and str(obj.product_photo).startswith('http'):
+            return str(obj.product_photo)
+        if obj.product_photo:
+            return self.context['request'].build_absolute_uri(obj.product_photo.url)
+        return None
+
+    def get_product_photo_small(self, obj):
+        if obj.product_photo and str(obj.product_photo).startswith('http'):
+            # Превью не делаем, возвращаем оригинал
+            return str(obj.product_photo)
+        if obj.product_photo_small:
+            return self.context['request'].build_absolute_uri(obj.product_photo_small.url)
+        return None
+
+    def get_product_photo_large(self, obj):
+        if obj.product_photo and str(obj.product_photo).startswith('http'):
+            return str(obj.product_photo)
+        if obj.product_photo_large:
+            return self.context['request'].build_absolute_uri(obj.product_photo_large.url)
+        return None
