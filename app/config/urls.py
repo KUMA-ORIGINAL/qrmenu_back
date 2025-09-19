a972708aea62ca178a54ce0fa48f5432dd6a153b
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-20 00:53:11 +0600

    feat: reset password

--- app/config/urls.py
+++ app/config/urls.py
@@ -43,4 +43,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+urlpatterns += [path('api/silk/', include('silk.urls', namespace='silk'))]
