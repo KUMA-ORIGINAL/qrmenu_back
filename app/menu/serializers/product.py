7f6257648847901ece5ee0c8bf747add14af9d0d
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-19 23:38:50 +0600

    feat: reset password

--- app/menu/serializers/product.py
+++ app/menu/serializers/product.py
@@ -39,9 +39,9 @@ class ProductSerializer(serializers.ModelSerializer):
     # product_attributes = ProductAttributeSerializer(many=True, read_only=True)
     modificators = ModificatorSerializer(many=True, read_only=True)
     category = CategoryShortSerializer(read_only=True)
-    product_photo = serializers.SerializerMethodField(read_only=True)
-    product_photo_small = serializers.SerializerMethodField(read_only=True)
-    product_photo_large = serializers.SerializerMethodField(read_only=True)
+    # product_photo = serializers.SerializerMethodField(read_only=True)
+    # product_photo_small = serializers.SerializerMethodField(read_only=True)
+    # product_photo_large = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Product
@@ -50,24 +50,24 @@ class ProductSerializer(serializers.ModelSerializer):
             'product_photo', 'product_photo_small', 'product_photo_large', 'category', 'is_recommended', 'modificators'
         ]
 
-    def get_product_photo(self, obj):
-        if obj.product_photo and str(obj.product_photo).startswith('http'):
-            return str(obj.product_photo)
-        if obj.product_photo:
-            return self.context['request'].build_absolute_uri(obj.product_photo.url)
-        return None
-
-    def get_product_photo_small(self, obj):
-        if obj.product_photo and str(obj.product_photo).startswith('http'):
-            # Превью не делаем, возвращаем оригинал
-            return str(obj.product_photo)
-        if obj.product_photo_small:
-            return self.context['request'].build_absolute_uri(obj.product_photo_small.url)
-        return None
-
-    def get_product_photo_large(self, obj):
-        if obj.product_photo and str(obj.product_photo).startswith('http'):
-            return str(obj.product_photo)
-        if obj.product_photo_large:
-            return self.context['request'].build_absolute_uri(obj.product_photo_large.url)
-        return None
+    # def get_product_photo(self, obj):
+    #     if obj.product_photo and str(obj.product_photo).startswith('http'):
+    #         return str(obj.product_photo)
+    #     if obj.product_photo:
+    #         return self.context['request'].build_absolute_uri(obj.product_photo.url)
+    #     return None
+    #
+    # def get_product_photo_small(self, obj):
+    #     if obj.product_photo and str(obj.product_photo).startswith('http'):
+    #         # Превью не делаем, возвращаем оригинал
+    #         return str(obj.product_photo)
+    #     if obj.product_photo_small:
+    #         return self.context['request'].build_absolute_uri(obj.product_photo_small.url)
+    #     return None
+    #
+    # def get_product_photo_large(self, obj):
+    #     if obj.product_photo and str(obj.product_photo).startswith('http'):
+    #         return str(obj.product_photo)
+    #     if obj.product_photo_large:
+    #         return self.context['request'].build_absolute_uri(obj.product_photo_large.url)
+    #     return None
